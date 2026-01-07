a9603004a6835bc8b792ce62cce2f1c4dc5e7888
itskkin <fyingpepper@gmail.com>
2026-01-08 02:39:47 +0800

    mao najud

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
@@ -10,21 +9,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: '/', // Important for Vercel
+  base: '/',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo) => {
-          let extType = assetInfo.name.split('.')[1]
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-            extType = 'images'
-          }
-          return `assets/${extType}/[name]-[hash][extname]`
-        }
-      }
-    }
+    emptyOutDir: true
   }
 })
